ce346c8ead07662407d32eff746b85fe88cb70c7
sdkraemer <tfcmaster9@gmail.com>
2017-02-08 09:17:49 -0700

    Remove docker port forwarding on mongo. Was messing with iptable and ufw firewall on ubuntu.

--- web/src/environments/environment.prod.ts
+++ web/src/environments/environment.prod.ts
@@ -1,5 +1,6 @@
 export const environment = {
   production: true,
   //apiUrl: "http://feeldaburnapi.azurewebsites.net/api"
-  apiUrl: "http://138.68.6.24/api"
+  //apiUrl: "http://138.68.6.24/api"
+  apiUrl: "http://xrcise.co/api"
 };
