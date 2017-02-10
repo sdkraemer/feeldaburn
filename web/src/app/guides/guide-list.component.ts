9e1b52df1837f3e9272e19c40519da1f7d02e436
sdkraemer <tfcmaster9@gmail.com>
2017-02-09 17:34:59 -0700

    Navbar, styling

--- web/src/app/guides/guide-list.component.ts
+++ web/src/app/guides/guide-list.component.ts
@@ -4,7 +4,13 @@ import { GuideService } from './guide.service';
 
 @Component({
     selector: 'guide-list',
-    templateUrl: 'guide-list.component.html'
+    templateUrl: 'guide-list.component.html',
+    styles: [`
+        .card {
+            margin-bottom: 1em;
+            width: 18rem;
+        }
+    `]
 })
 export class GuideListComponent implements OnInit {
     guides: Guide[];
