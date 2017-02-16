584fe0362b85b946512fff23ded356909a823504
sdkraemer <tfcmaster9@gmail.com>
2017-02-15 21:05:27 -0700

    remove home import on app route

--- web/src/app/app.routes.ts
+++ web/src/app/app.routes.ts
@@ -1,7 +1,6 @@
 import { ModuleWithProviders }      from '@angular/core';
 import { Routes, RouterModule }     from '@angular/router';
 
-import { HomeComponent }            from './home.component';
 
 import { WorkoutsComponent }        from './workouts/workouts.component';
 import { WorkoutComponent }         from './workouts/workout.component';
