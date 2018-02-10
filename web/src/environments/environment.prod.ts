358c67440ef2ec46ea99d0f532f69eb50a0f914a
sdkraemer <tfcmaster9@gmail.com>
2018-02-10 14:43:19 -0700

    .

--- web/src/environments/environment.prod.ts
+++ web/src/environments/environment.prod.ts
@@ -2,5 +2,6 @@ export const environment = {
   production: true,
   //apiUrl: "http://feeldaburnapi.azurewebsites.net/api"
   //apiUrl: "http://138.68.6.24/api"
-  apiUrl: "http://xrcise.co/api"
+  //apiUrl: "http://xrcise.co/api"
+  apiUrl: "/api"
 };
--- web/src/environments/environment.ts
+++ web/src/environments/environment.ts
@@ -5,5 +5,5 @@
 
 export const environment = {
   production: false,
-  apiUrl: "http://localhost/api"
+  apiUrl: "/api"
 };
