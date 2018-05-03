8a29069a12c0da630206be755f148f093cbd0db6
Scott Kraemer <skraemer@CLG.Local>
2018-05-03 15:59:34 -0700

    refactoring

--- web/src/app/workouts/workout-tracker/workoutfactory.service.ts
+++ web/src/app/workouts/workout-tracker/workoutfactory.service.ts
@@ -1,11 +1,16 @@
 import { Injectable } from "@angular/core";
 import { Observable } from "rxjs/Observable";
 
-import { IWorkout, IStrengthTrainingWorkout } from "../../core";
+import {
+  IWorkout,
+  IStrengthTrainingWorkout,
+  StrengthTrainingWorkout
+} from "../../core";
 import { IGuide, Guide } from "../../core";
 import { WorkoutService } from "app/workouts/workout.service";
 import { GuideService } from "app/guides/guide.service";
 import { WorkoutFactory } from "app/core/factories/workoutfactory";
+import { map } from "rxjs/operators";
 
 @Injectable()
 export class WorkoutFactoryService {
