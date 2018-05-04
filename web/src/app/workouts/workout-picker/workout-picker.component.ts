674cd33a5f00b273facd0bf6f680ccda2c4494e1
Scott Kraemer <skraemer@CLG.Local>
2018-05-04 09:59:03 -0700

    Making workout-picker responsive

--- web/src/app/workouts/workout-picker/workout-picker.component.ts
+++ web/src/app/workouts/workout-picker/workout-picker.component.ts
@@ -1,44 +1,40 @@
-import { Component, OnInit } from '@angular/core';
-import { FormArray, FormBuilder, Validators, FormGroup } from '@angular/forms';
-import { ActivatedRoute, Router } from '@angular/router';
+import { Component, OnInit } from "@angular/core";
+import { FormArray, FormBuilder, Validators, FormGroup } from "@angular/forms";
+import { ActivatedRoute, Router } from "@angular/router";
 
 @Component({
-    selector: 'workout-picker',
-    templateUrl: 'workout-picker.component.html',
-    styles: [`
-        .card-layout {
-            min-width: 50%;
-        }
-    `],
+  selector: "workout-picker",
+  templateUrl: "workout-picker.component.html",
+  styles: [
+    `
+    `
+  ]
 })
 export class WorkoutPickerComponent implements OnInit {
-    public form: FormGroup;
+  public form: FormGroup;
 
-    public workoutTypes = [
-        { value: 'STRENGTH_TRAINING', display: 'Strength Training' },
-        { value: 'RUNNING', display: 'Running' },
-        { value: 'CYCLING', display: 'Cycling' }
-    ];
+  public workoutTypes = [
+    { value: "STRENGTH_TRAINING", display: "Strength Training" },
+    { value: "RUNNING", display: "Running" },
+    { value: "CYCLING", display: "Cycling" }
+  ];
 
-    constructor(
-        private formBuilder: FormBuilder,
-        private router: Router
-    ) { }
+  constructor(private formBuilder: FormBuilder, private router: Router) {}
 
-    ngOnInit() { 
-        this.form = this.formBuilder.group({
-            workoutType: [''],
-            guide: ['']
-        });
+  ngOnInit() {
+    this.form = this.formBuilder.group({
+      workoutType: [""],
+      guide: [""]
+    });
+  }
+
+  startWorkout() {
+    var routingParameters = {
+      workoutType: this.form.value.workoutType
+    };
+    if (routingParameters.workoutType == "STRENGTH_TRAINING") {
+      routingParameters["guide"] = this.form.value.guide;
     }
-
-    startWorkout() {
-        var routingParameters = {
-            workoutType: this.form.value.workoutType
-        };
-        if(routingParameters.workoutType == 'STRENGTH_TRAINING'){
-            routingParameters['guide'] = this.form.value.guide
-        }
-        this.router.navigate(['/workout-tracker', routingParameters]);
-    }
+    this.router.navigate(["/workout-tracker", routingParameters]);
+  }
 }
