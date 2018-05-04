acbbc091cf8dff88980a5f6074bf71d2e8c34f3b
Scott Kraemer <skraemer@CLG.Local>
2018-05-04 10:09:05 -0700

    Removed unused component

--- web/src/app/guides/guides.module.ts
+++ web/src/app/guides/guides.module.ts
@@ -1,29 +1,17 @@
-import { NgModule } from '@angular/core';
-import { ReactiveFormsModule } from '@angular/forms';
+import { NgModule } from "@angular/core";
+import { ReactiveFormsModule } from "@angular/forms";
 
-import { GuidesRoutingModule, routedComponents } from './guides.routing';
+import { GuidesRoutingModule, routedComponents } from "./guides.routing";
 
-import { SharedModule } from '../shared/shared.module';
-import { GuideService } from './guide.service';
+import { SharedModule } from "../shared/shared.module";
+import { GuideService } from "./guide.service";
 
-import { GuideListComponent } from './guide-list.component';
-import { ExerciseTypeControlComponent } from "app/guides/exercise-type-control.component";
-
+import { GuideListComponent } from "./guide-list.component";
 
 @NgModule({
-    imports: [
-        SharedModule,
-        GuidesRoutingModule,
-        ReactiveFormsModule
-    ],
-    exports: [],
-    declarations: [
-        GuideListComponent,
-        ExerciseTypeControlComponent,
-        routedComponents
-    ],
-    providers: [
-        GuideService
-    ],
+  imports: [SharedModule, GuidesRoutingModule, ReactiveFormsModule],
+  exports: [],
+  declarations: [GuideListComponent, routedComponents],
+  providers: [GuideService]
 })
-export class GuidesModule { }
+export class GuidesModule {}
